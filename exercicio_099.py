9712921ca73dfa31e2354aefdc81842bb0bef3d3
Fabio JS Pereira <fabiojspereira@gmail.com>
2022-03-15 15:14:34 -0300

    Lista de programas atualizados ...

--- exercicio_099.py
+++ exercicio_099.py
@@ -3,10 +3,13 @@ from time import sleep
 def maior_numero (* numeros):
 	print()
 	print("*" * 80)
-	print("ANALIZANDO OS VALORES PASSADOS ...")
+	print("ANALISANDO OS VALORES PASSADOS ...")
 	qtd_numeros = len(numeros)
 
-	print(f"Os seguintes números foram passados : ",end="")
+	print(f"Os seguintes números foram passados : ", end="")
+	if qtd_numeros == 0:
+		print(end="\033[1;31m FIM DA LISTA\033[m")
+
 	contador = 1
 	for valor in numeros:
 		sleep(0.25)
@@ -14,7 +17,7 @@ def maior_numero (* numeros):
 			print(f"\033[1;34m{valor}\033[m", end=", ")
 			contador += 1
 		else:
-			print(f"\033[1;34m{valor}\033[m",end="\033[1;31m FIM DA LISTA\033[m")
+			print(f"\033[1;34m{valor}\033[m", end="\033[1;31m FIM DA LISTA\033[m")
 			contador += 1
 
 	print(f"\nTotalizando : {len(numeros)} números.")
@@ -29,7 +32,9 @@ def maior_numero (* numeros):
 	print("*" * 80)
 
 maior_numero(12, -1, 2, 23, 0, 7, 8, 9, 12)
-maior_numero(101,54, 56, 12, 1, 27, 55, 100)
+maior_numero(101,54, 56, 1, 27, 55, 100)
 maior_numero(2, 3)
 maior_numero(15)
 maior_numero(-2, -1, 0)
+maior_numero(1)
+maior_numero()
