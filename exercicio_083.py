ea76a29bc07029424b5672280ff8617c7e800c97
fabiojspereira <fabiojspereira@gmail.com>
2023-01-26 13:25:02 -0300

    Update

--- exercicio_083.py
+++ exercicio_083.py
@@ -1,23 +1,65 @@
 parenteses = []
+erro = False
 
-exp = str(input("Digite a expressão : ")).strip()
+while True:
+	exp = str(input("Digite a expressão : ")).strip()
 
-for char in range(0, len(exp)):
-	if exp[char] == "(":
-		parenteses.append("(")
-	elif exp[char] == ")":
-		if len(parenteses) > 0:
-			parenteses.pop()
-		else :
-			parenteses.append(")")
+	for char in range(0, len(exp)):
+		if exp[0] == ")":
+			print("ERROR")
+			erro = True
 			break
 
-if len(parenteses) == 0:
-	print(f"\nA expressão {exp} é verdadeira !")
-else :
-	print(f"A expressão {exp} é falsa !")
+		if exp[char] == "(":
+			parenteses.append("(")
+		elif exp[char] == ")":
+			if len(parenteses) > 0:
+				parenteses.pop()
+			else:
+				parenteses.append(")")
+				break
+
+	if erro == False:
+		if len(parenteses) == 0:
+			print(f"\nA expressão {exp} é verdadeira !\n")
+		else:
+			print(f"A expressão {exp} é falsa !\n")
+		parenteses = []  # É necessário limpar a lista neste ponto.
+	else:
+		break
+
+
 
 '''
+parenteses = []
+continua = True
+
+while continua == True:
+	exp = str(input("Digite a expressão : ")).strip()
+	if exp[0] == ")":
+		print("Expressão iniciou de forma indevida.")
+		continua = False
+
+	for char in range(0, len(exp)):
+		if exp[char] == "(":
+			parenteses.append("(")
+		elif exp[char] == ")":
+			if len(parenteses) > 0:
+				parenteses.pop()
+			else:
+				parenteses.append(")")
+				break
+	continua = True
+
+	if len(parenteses) == 0:
+		print(f"\nA expressão {exp} é verdadeira !\n")
+	else:
+		print(f"A expressão {exp} é falsa !\n")
+	parenteses = []  # É necessário limpar a lista neste ponto.
+
+
+
+
 (a*b)-)c(=12
 
 
